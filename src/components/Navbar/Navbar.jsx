ab1ff0806002997c62f94f49c7b7f679be11b767
indrajeetbhattacharya <indrajeetbhattacharya5@gmail.com>
2023-01-09 18:39:47 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,8 @@
-import React,{useState} from "react";
-
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
-import images from "../../constants/images";
-
-import "./Navbar.css";
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
+import './Navbar.css';
 
 const Navbar = () => {
 
